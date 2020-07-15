2d17aa08ff3e06fa018809bcbe00cdb95f02bd5b
Michael de Oliveira Ferreira <oliveiramicahel@outlook.com>
2020-07-15 14:42:39 -0300

    Teste e mais testes

--- database/migrations/2020_07_15_144051_rename_fields_invoice.php
+++ database/migrations/2020_07_15_144051_rename_fields_invoice.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class RenameInvoiceFields extends Migration
+class RenameFieldsInvoice extends Migration
 {
     public function up()
     {
