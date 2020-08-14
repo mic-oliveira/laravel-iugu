3857c12ce98dd570fd32515d7827987897329126
Michael de Oliveira Ferreira <oliveiramicahel@outlook.com>
2020-08-14 12:04:24 -0300

    Renomear charge

---
+++ database/migrations/2020_08_14_144000_rename_dates_fields_Invoice.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class RenameDatesFieldsInvoice extends Migration
+{
+    public function up()
+    {
+        Schema::connection(config('iugu.connection'))->table('invoices', function (Blueprint $table) {
+            $table->renameColumn('captured_at','captured_at_iso');
+            $table->renameColumn('authorized_at','authorized_at_iso');
+            $table->renameColumn('expired_at','expired_at_iso');
+            $table->renameColumn('refunded_at','refunded_at_iso');
+            $table->renameColumn('canceled_at','canceled_at_iso');
+            $table->renameColumn('chargeback_at','chargeback_at_iso');
+
+        });
+    }
+
+    public function down()
+    {
+        Schema::connection(config('iugu.connection'))->table('invoices',function (Blueprint $table) {
+            $table->renameColumn('captured_at_iso','captured_at');
+            $table->renameColumn('authorized_at_iso','authorized_at');
+            $table->renameColumn('expired_at_iso','expired_at');
+            $table->renameColumn('refunded_at_iso','refunded_at');
+            $table->renameColumn('canceled_at_iso','canceled_at');
+            $table->renameColumn('chargeback_at_iso','chargeback_at');
+        });
+    }
+
+}
--- database/migrations/2020_08_14_144051_fix_dates_fields_Invoice.php
+++ database/migrations/2020_08_14_144051_fix_dates_fields_Invoice.php
@@ -9,12 +9,6 @@ class FixDatesFieldsInvoice extends Migration
     public function up()
     {
         Schema::connection(config('iugu.connection'))->table('invoices', function (Blueprint $table) {
-            $table->renameColumn('captured_at','captured_at_iso');
-            $table->renameColumn('authorized_at','authorized_at_iso');
-            $table->renameColumn('expired_at','expired_at_iso');
-            $table->renameColumn('refunded_at','refunded_at_iso');
-            $table->renameColumn('canceled_at','canceled_at_iso');
-            $table->renameColumn('chargeback_at','chargeback_at_iso');
             $table->string('captured_at')->after('paid')->nullable();
             $table->string('authorized_at')->after('captured_at_iso')->nullable();
             $table->string('expired_at')->after('authorized_at_iso')->nullable();
@@ -37,13 +31,6 @@ class FixDatesFieldsInvoice extends Migration
             $table->dropColumn('protested_at');
             $table->dropColumn('chargeback_at');
             $table->dropColumn('ocurrence_date');
-            $table->renameColumn('captured_at_iso','captured_at');
-            $table->renameColumn('authorized_at_iso','authorized_at');
-            $table->renameColumn('expired_at_iso','expired_at');
-            $table->renameColumn('refunded_at_iso','refunded_at');
-            $table->renameColumn('canceled_at_iso','canceled_at');
-            $table->renameColumn('protested_at_iso','protested_at');
-            $table->renameColumn('chargeback_at_iso','chargeback_at');
         });
     }
 
