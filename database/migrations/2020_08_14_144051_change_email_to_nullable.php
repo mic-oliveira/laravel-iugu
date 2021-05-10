4cd98827b1815ec75ee1b1b52e3ac77f680d1d76
Michael de Oliveira Ferreira <oliveiramichael@outlook.com>
2021-05-10 17:47:31 -0300

    adicao do campo pix em invoice

---
+++ database/migrations/2020_08_14_144051_change_email_to_nullable.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ChangeEmailToNullable extends Migration
+{
+    public function up()
+    {
+        Schema::connection(config('iugu.connection'))->table('invoices', function (Blueprint $table) {
+            $table->string('email')->nullable();
+        });
+    }
+
+    public function down()
+    {
+        Schema::connection(config('iugu.connection'))->table('invoices',function (Blueprint $table) {
+            $table->string('email')->nullable(false);
+        });
+    }
+
+}
