017a67bbd7b83db39b2c24abd53826f746b20f7d
Michael de Oliveira Ferreira <oliveiramichael@outlook.com>
2021-05-10 14:30:19 -0300

    payable_with na tabela invoice

---
+++ database/migrations/2020_08_14_144051_rename_payment_method_to_payable_with.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class RenamePaymentMethidToPayableWith extends Migration
+{
+    public function up()
+    {
+        Schema::connection(config('iugu.connection'))->table('invoices', function (Blueprint $table) {
+            $table->renameColumn('payment_method','payable_with');
+        });
+    }
+
+    public function down()
+    {
+        Schema::connection(config('iugu.connection'))->table('invoices',function (Blueprint $table) {
+            $table->renameColumn('payable_with','payment_method');
+        });
+    }
+
+}
